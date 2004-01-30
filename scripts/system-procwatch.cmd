ec3caa1fef64757e25745cf79aab388f23530954
Michael Wallner <mike@php.net>
2004-01-30 17:12:00 +0000

    * oops, added args placeholders
    git-svn-id: http://svn.php.net/repository/pear/packages/System_ProcWatch/trunk@150172 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/system-procwatch.cmd
+++ scripts/system-procwatch.cmd
@@ -2,5 +2,5 @@
 REM
 REM procwatch.cmd
 REM
-@PHP-BIN@ @BIN-DIR@\procwatch
+@PHP-BIN@ @BIN-DIR@\procwatch %1 %2 %3 %4 %5 %6 %7 %8 %9
 @ECHO ON
--- scripts/system-procwatch_lint.cmd
+++ scripts/system-procwatch_lint.cmd
@@ -2,5 +2,5 @@
 REM
 REM procwatch-lint
 REM
-@PHP-BIN@ @BIN-DIR@\procwatch-lint
+@PHP-BIN@ @BIN-DIR@\procwatch-lint %1 %2 %3 %4 %5 %6
 @ECHO ON
